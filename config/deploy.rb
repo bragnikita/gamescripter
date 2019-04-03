f9a5308bebbff0a4aa4a1f3f5ae737a9735d2934
bragnikita <bragnikita@users.noreply.github.com>
2019-04-03 16:53:33 +0900

    deploy config updated

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,14 +7,14 @@ set :repo_url, "git@github.com:bragnikita/gamescripter.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 set :branch, "master"
 
-set :deploy_to, "/home/ec2-user/api"
+set :deploy_to, "/tmp/www/#{fetch(:application)}"
 
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
 set :pty,             false
 set :stage,           :production
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_bind,       "unix:///tmp/gamescripter_api.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
