e2586bc5f35835af04c87349be9ecb90064bd75c
bragnikita <bragnikita@users.noreply.github.com>
2019-04-03 19:34:28 +0900

    +deploy keys

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,13 +3,16 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "__bragnikita_gamescripter-api",
-       roles: %w{app},
-       default_env: { APP_ENV: "production" }
+# server "__bragnikita_gamescripter-api",
+#        roles: %w{app},
+#        default_env: { APP_ENV: "production" }
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server ENV['DEPLOY_HOST'],
+       user: "ec2-user",
+       roles: %w{app},
+       default_env: { APP_ENV: "production" }
 
 # role-based syntax
 # ==================
@@ -24,7 +27,6 @@ server "__bragnikita_gamescripter-api",
 # role :db,  %w{deploy@example.com}
 
 
-
 # Configuration
 # =============
 # You can set any configuration variable like in config/deploy.rb
@@ -32,7 +34,6 @@ server "__bragnikita_gamescripter-api",
 # For available Capistrano configuration variables see the documentation page.
 # http://capistranorb.com/documentation/getting-started/configuration/
 # Feel free to add new variables to customise your setup.
-
 
 
 # Custom SSH Options
@@ -51,13 +52,17 @@ server "__bragnikita_gamescripter-api",
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
+#
+
+# set :keyfile, File.expand_path('../server_key', File.dirname(__FILE__))
+#
+# set :ssh_options, { keys: [fetch(:keyfile)] } if File.exist?(fetch(:keyfile))
 # server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
+#        user: "ec2-user",
+#        roles: %w{app},
+#        default_env: { APP_ENV: "production" },
+#        ssh_options: {
+#          user: "ec2-user", # overrides user setting aboves
+#          forward_agent: true,
+#          auth_methods: %w(publickey password)
+#        }
