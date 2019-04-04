0cfa4c7d9f48e8c719ef348ae220a2215332d69d
bragnikita <bragnikita@users.noreply.github.com>
2019-04-04 12:13:12 +0900

    deployment: switched to after_success hook

--- config/cap_deploy.sh
+++ config/cap_deploy.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
+set -e
 
+[[ $TRAVIS_BRANCH != "$1" ]] && exit 0
+
+openssl aes-256-cbc -k ${DEPLOY_KEY} -in config/server_key_enc -d -a -out config/server_key
+eval "$(ssh-agent -s)"
+chmod 600 config/server_key
+ssh-add config/server_key
+bundle exec cap production deploy:check
 bundle exec cap production deploy
+
+echo "======== Deploy success =========="
--- config/cap_deploy_prepare.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-openssl aes-256-cbc -k ${DEPLOY_KEY} -in config/server_key_enc -d -a -out config/server_key
-eval "$(ssh-agent -s)"
-chmod 600 config/server_key
-ssh-add config/server_key
-bundle exec cap production deploy:check
